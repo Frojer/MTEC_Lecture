330c46ce38cdcb6472ceffd16d9784162fb5561c
Frojer <choicoco1995@naver.com>
2016-09-07 10:16:17 +0900

    수업 1일
    - 유니티 프로젝트 만들기
    - MyComponent 만들기

---
+++ Assets/Lecture/Scripts/Editor/MyComponentEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MyComponent))]
+public class MyComponentEditor : Editor
+{
+    void OnEnable()
+    {
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        MyComponent myComponent = (MyComponent)target;
+        
+        myComponent.intVariable = EditorGUILayout.IntField("Int Variable", myComponent.intVariable);
+        myComponent.floatVariable = EditorGUILayout.Slider("Float Variable", myComponent.floatVariable, 0.0f, 100.0f);
+        myComponent.IntVar = EditorGUILayout.IntField("Int Var", myComponent.intVariable);
+
+        if (GUILayout.Button("Do something"))
+        {
+            myComponent.DoSomething();
+        }
+    }
+}

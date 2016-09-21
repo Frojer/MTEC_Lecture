efc1b5586775ecc61c07fd1cd1cc4e074ff2496d
Frojer <choicoco1995@naver.com>
2016-09-21 12:47:41 +0900

    Day3. 수업
    -Custom Scene 디스플레이 방법
    -Custom Asset 만들기
    -Midi File Importer 구현
    -Midi File 라이브러리 포함

---
+++ Assets/Lecture/Scripts/Editor/PathNodeEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(PathNode))]
+public class PathNodeEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        PathNode pathNode = (PathNode)target;
+
+        Handles.color = Color.white;
+        Handles.Label(pathNode.transform.position, pathNode.name);
+    }
+}
---
+++ Assets/Lecture/Scripts/PathNode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathNode : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        Vector3 fromVec = transform.position + transform.forward * 1.0f;
+        Vector3 toVec = transform.position + transform.forward * -1.0f;
+
+        Gizmos.DrawLine(fromVec, toVec);
+    }
+}

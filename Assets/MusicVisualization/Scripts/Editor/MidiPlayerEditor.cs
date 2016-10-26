aec433707613d1b7a83ca1a0609eb567aa21db5a
Frojer <choicoco1995@naver.com>
2016-10-26 12:19:36 +0900

    Day7 수업
    - MidiPlayer 완성

---
+++ Assets/MusicVisualization/Scripts/Editor/MidiPlayerEditor.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MidiPlayer))]
+public class MidiPlayerEditor : Editor
+{
+    SerializedProperty midi;
+    SerializedProperty music;
+    SerializedProperty audioSource;
+    SerializedProperty playDelayTime;
+    SerializedProperty playSpeed;
+
+    void OnEnable()
+    {
+        midi = serializedObject.FindProperty("midi");
+        music = serializedObject.FindProperty("music");
+        audioSource = serializedObject.FindProperty("audioSource");
+        playDelayTime = serializedObject.FindProperty("playDelayTime");
+        playSpeed = serializedObject.FindProperty("playSpeed");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(midi);
+        EditorGUILayout.PropertyField(music);
+        EditorGUILayout.PropertyField(audioSource);
+        EditorGUILayout.PropertyField(playDelayTime);
+        EditorGUILayout.PropertyField(playSpeed);
+
+        serializedObject.ApplyModifiedProperties();
+
+        MidiPlayer midiPlayer = (MidiPlayer)target;
+
+        if (Application.isPlaying)
+        {
+            GUILayout.BeginHorizontal();
+
+            if (midiPlayer.isPlaying)
+            {
+                if (GUILayout.Button("Pause"))
+                {
+                    midiPlayer.Pause();
+                }
+
+                EditorUtility.SetDirty(target);
+            }
+
+            else
+            {
+                if (midiPlayer.playTime == 0.0f)
+                {
+                    if (GUILayout.Button("Play"))
+                    {
+                        midiPlayer.Play();
+                    }
+                }
+
+                else
+                {
+                    if (GUILayout.Button("Resume"))
+                    {
+                        midiPlayer.Resume();
+                    }
+                }
+            }
+
+            if (GUILayout.Button("Stop"))
+            {
+                midiPlayer.Stop();
+            }
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.HorizontalSlider(midiPlayer.playTime, 0.0f, midiPlayer.totalTime);
+            EditorGUILayout.LabelField(string.Format("Time: {0:f1}sec", midiPlayer.playTime));
+        }
+    }
+}
